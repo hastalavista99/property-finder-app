7f7a66f14c7273188edaa98a40052c8e915adcb6
hastalavista <githumuj@gmail.com>
2023-05-08 16:03:59 +0300

    solved devise errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :properties
+  devise_for :accounts
+  #get 'home/index'
+
+  root to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
